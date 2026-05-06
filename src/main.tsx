34db0f8c63d86c0f11848cd089efbbe48f3175b9
PedroLucasBastos <pedro19792001@gmail.com>
2026-05-06 12:56:43 -0300

    chore: Intalando metricas de desempenho da vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { GoogleOAuthProvider } from '@react-oauth/google'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import './assets/styles/global.css'
 import App from './App.tsx'
 
@@ -8,6 +10,8 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
       <App />
+      <Analytics />
+      <SpeedInsights />
     </GoogleOAuthProvider>
   </StrictMode>,
 )
